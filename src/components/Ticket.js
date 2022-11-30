51454617e71943d5bc44f45e0b2414e30edca979
jakechaley <jakechaley@gmail.com>
2022-11-30 15:14:02 -0800

    begin adding static components

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-function App(){
+function Ticket() {
   const name1 = "Thato";
-  const name2 = "Haley"
+  const name2 = "Haley";
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
       <h3>3a</h3>
       <h3>{name1} and {name2}</h3>
       <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <hr />
     </React.Fragment>
   );
 }
 
-export default App;
+export default Ticket;
