422705ac15cb169954155cba38e1701e4cc5cc58
jakechaley <jakechaley@gmail.com>
2022-11-30 16:25:44 -0800

    static application mapping throw ticket array

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,19 +1,36 @@
 import React from "react";
 import Ticket from "./Ticket";
 
+const mainTicketList = [
+  {
+    names: "Thato and Haley",
+    location: "3A",
+    issue: "Firebase won't save record."
+  },
+  {
+    names: "Sleater and Kinney",
+    location: "4B",
+    issue: "Prop types are throwing an error."
+  },
+  {
+    names: "Imani and Jacob",
+    location: "9F",
+    issue: "Child component isn't rendering."
+  }
+];
+
 function TicketList() {
   return (
     <React.Fragment>
-      <Ticket 
-        location="3A"
-        names="Thato and Haley"
-        issues="Firebase will not save record!"
-      />
-      <Ticket
-        location="4B"
-        names="Sleater and Kinney"
-        issues="Prop types are throwing an error."
-      />
+      <hr />
+      {mainTicketList.map((ticket, index) => 
+        <Ticket
+        names={ticket.names}
+        location={ticket.location}
+        issues={ticket.issue}
+        key={index}
+        />
+      )}
     </React.Fragment>
   );
 }
